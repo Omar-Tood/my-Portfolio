b350310752483a2f538a1746c0defe32bdb0a430
Engr-Pirlo <omarjibrilabdulkhadir@gmail.com>
2023-07-20 12:28:39 +0300

    first commit

---
+++ src/components/Hireme.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import hireMe from "../assets/images/Hireme1.png";
+const Hireme = () => {
+  return (
+    <section id="hireme" className="py-10 px-3 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-semibold">
+          Hire <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">Do you have any work?</p>
+      </div>
+      <div className="bg-gray-700 relative px-8 rounded-2xl py-5 lg:max-w-4xl mx-auto min-h-[24rem] mt-24 flex gap-6 lg:flex-row flex-col-reverse items-center">
+        <div>
+          <h2 className="text-2xl font-semibold">
+            Do you want any work from me?
+          </h2>
+          <p className="lg:text-left text-justify max-w-lg text-sm mt-4 text-gray-200 leading-6">
+            If you have a job you can give me because I am qualified for this
+            job because of my experience so I hope you will hire me I am sure I
+            will get this job as expected and I will work for my value and I
+            will not get paid!😘
+          </p>
+          <button className="btn-primary mt-10">Say Hello</button>
+        </div>
+        <img
+          src={hireMe}
+          alt=""
+          className="lg:h-[32rem] h-80 lg:absolute bottom-0 -right-3 object-cover"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hireme;
---
+++ src/components/Project.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import project1 from "../assets/images/Expense-tracker-projec1.png";
+import project2 from "../assets/images/fecbook-clone-projec3.png";
+import project3 from "../assets/images/Linked-clone-projec2.png";
+
+import project_person from "../assets/images/Hireme1.png";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper";
+
+const Project = () => {
+  const projects = [
+    {
+      img: project1,
+      name: "Expense Tracke Manager",
+      github_link: "https://github.com/Engr-Pirlo/Expense-Tracker",
+      live_link: "expense-tracker-black-pi.vercel.app",
+    },
+    {
+      img: project2,
+      name: "Fecbook-Clone",
+      github_link: "https://github.com/Engr-Pirlo/Facebook-Clone",
+      live_link: "https://facebook-clone-beryl-five.vercel.app/",
+    },
+    {
+      img: project3,
+      name: "Linked-Clone",
+      github_link: "https://github.com/Engr-Pirlo/Linked-Clone",
+      live_link: "https://linked-clone12.vercel.app/",
+    },
+   
+  ];
+  return (
+    <section id="projects" className="py-10 text-white">
+      <div className="text-center">
+        <h3 className="text-4xl font-semibold">
+          My <span className="text-cyan-600">Projects</span>
+        </h3>
+        <p className="text-gray-400 mt-3 text-lg">My awesome works</p>
+      </div>
+      <br />
+      <div className="flex max-w-6xl gap-6 px-5 mx-auto items-center relative">
+        <div className="lg:w-2/3 w-full">
+          <Swiper
+            slidesPerview={1.2}
+            spaceBetween={20}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+            }}
+            loop={true}
+            autoplay={{
+              delay: 3000,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination, Autoplay]}
+          >
+            {projects.map((project_info, i) => (
+              <SwiperSlide key={i}>
+                <div className="h-fit w-full p-4 bg-slate-700 rounded-xl">
+                  <img src={project_info.img} alt="" className="rounded-lg" />
+                  <h3 className="text-xl my-4">{project_info.name}</h3>
+                  <div className="flex gap-3">
+                    <a
+                      href={project_info.github_link}
+                      target="_blank"
+                      className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                    >
+                      Github
+                    </a>
+                    <a
+                      href={project_info.live_link}
+                      target="_blank"
+                      className="text-cyan-600 bg-gray-800 px-2 py-1 inline-block"
+                    >
+                      Live Demo
+                    </a>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="lg:block hidden">
+          <img src={project_person} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Project;
